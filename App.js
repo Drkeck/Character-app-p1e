f3532ac3077292ab8a6757633cae9027038084d3
Drkeck <k3ckl3y.a@gmail.com>
2021-06-17 01:11:42 -0700

    we're starting this react native journy.. i hope i can make something cool

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Lets make this a Pathfinder 1e app for new players</Text>
       <StatusBar style="auto" />
     </View>
   );

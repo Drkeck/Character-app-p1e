e7c7461b9d5a209ff1389c8b485adea6890e85de
Drkeck <k3ckl3y.a@gmail.com>
2021-07-19 11:14:54 -0700

    added a example json of what the saved character sheet is supposed to look like.

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Lets make this a Pathfinder 1e app for new players</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto"/>
     </View>
   );
 }
